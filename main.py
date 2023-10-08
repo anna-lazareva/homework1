d83dfdf305eb72f180edd427a4b3d323bedba80c
Alexandr Lazarev <alexandr.lazarev@teko.biz>
2023-10-08 21:43:39 +0300

    upd homework

--- main.py
+++
@@ -1,9 +0,0 @@
-children = 30
-if children == 35:
-    print('35 детей')
-if children < 35:
-    print('детей меньше 35')
-if children > 35:
-    print('детей больше 35')
-elif children >= 35:
-    print('детей больше или равно 35')
